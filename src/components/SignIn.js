251137d2f03751feba418d599db70542e0a85f61
Steven <stevj17@gmail.com>
2019-06-12 23:11:51 -0700

    Swedish translations

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Redirect } from 'react-router-dom';
 
 class SignIn extends Component{
@@ -10,18 +10,27 @@ class SignIn extends Component{
         };
     }
    render(){
+       const headerStyles = {
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '50%',
+        marginTop: '75px',
+        opacity: 0.95,
+       }
        const styles = {
-        width: '300px',
-        height: '200px',
+        width: '350px',
+        height: '250px',
         padding: '20px',
         margin: '100px auto',
+        // marginTop: '250px',
         boxShadow: '0px 0px 10px 2px grey'
        }
        const submitStyle = {
         fontFamily: "Roboto, sans-serif",
         textTransform: 'uppercase',
         outline: 0,
-        background: 'gold',
+        background: '#0051BA',
         width: '100%',
         border: 0,
         padding: '15px',
@@ -42,13 +51,16 @@ class SignIn extends Component{
        }
        if(this.state.route !== '') return <Redirect to={this.state.route}/>
        return (
-        <div className='login-wrap' style={styles}>          
-            <input style={inputStyle} placeholder='Username' type='name'name='email'/>
-            <input style={inputStyle} placeholder='Password' type='password' name='password'/>
-            <input style={submitStyle} type='submit' value='login'/>
-            <button onClick={()=>{this.state.changeRoute('/landing')}}>CLICK TO GO TO LANDING</button>
+        <Fragment>
+            <img src="https://fontmeme.com/permalink/190613/3ceff503b7fdbbdb4c0fbf5ededcf6a7.png" className="img-rotate-scale" style={headerStyles}/>
+        <div className='login-wrap' style={styles}>               
+            <input style={inputStyle} placeholder='Användarnamn' type='name'name='email'/>
+            <input style={inputStyle} placeholder='Lösenord' type='password' name='password'/>
+            <input onClick={()=>{this.state.changeRoute('/canvas')}} style={submitStyle} type="submit" value='Logga in'/>
+            {/* <button onClick={()=>{this.state.changeRoute('/landing')}}>CLICK TO GO TO LANDING</button> */}
             <button onClick={()=>{this.state.changeRoute('/canvas')}}>CLICK TO GO TO CANDYLAND</button>
         </div>
+        </Fragment>
        );
    }
 }
