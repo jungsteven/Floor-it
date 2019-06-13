8da5c9c933ad97eb67782fda1349c3eb149980e6
Steven <stevj17@gmail.com>
2019-06-12 21:08:23 -0700

    sidebar fixed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,12 +13,12 @@ class Sidebar extends Component{
          <Fragment>
            <nav className="side-bar">
               <ul className="side-ul">
-                <li className="side-li"><a href="">Bed<span><i className="fas fa-cat"></i></span></a></li>
-                <li className="side-li"><a href="">Vase<span><i className="fas fa-cat"></i></span> </a></li>
-                <li className="side-li"><a href="">Couch<span><i className="fas fa-cat"></i></span> </a></li>
-                <li className="side-li"><a href="">TV<span><i className="fas fa-cat"></i></span> </a></li>
-                <li className="side-li"><a href="">Wardrobe<span><i className="fas fa-cat"></i></span> </a></li>
-                <li className="side-li"><a href="">Cat<span><i className="fas fa-cat"></i></span> </a></li>
+                <li className="side-li"><a href="">Bed<span className="spam"><i className="fas fa-cat"></i></span></a></li>
+                <li className="side-li"><a href="">Vase<span className="spam"><i className="fas fa-cat"></i></span> </a></li>
+                <li className="side-li"><a href="">Couch<span className="spam"><i className="fas fa-cat"></i></span> </a></li>
+                <li className="side-li"><a href="">TV<span className="spam"><i className="fas fa-cat"></i></span> </a></li>
+                <li className="side-li"><a href="">Wardrobe<span className="spam"><i className="fas fa-cat"></i></span> </a></li>
+                <li className="side-li"><a href="">Cat<span className="spam"><i className="fas fa-cat"></i></span> </a></li>
               </ul>
             </nav>
          </Fragment>
